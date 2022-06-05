550ad80a70000a12a631d2f91d0907b282e263c2
Daniel Harianja <danieltogarharianja5@gmail.com>
2022-06-06 01:25:21 +0700

    set port to 3000 for glitch

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ app.use( cors() );
 app.use(indexRoutes);
 app.use(searchRoutes);
 
-app.listen( 6969, () => console.log( 'listening on port 3000...' ) )
+app.listen( 3000, () => console.log( 'listening on port 3000...' ) )
